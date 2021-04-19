a43ed21aca9c05ce3a0c09cfe599b2297db8c92d
Mrigank Pawagi <mrigankpawagi@gmail.com>
2021-04-19 23:19:09 +0530

    init

---
+++ js/init.js
@@ -0,0 +1,9 @@
+function load(time=150, delay=0){
+    $('#loader').delay(delay).fadeIn(time);   
+}
+function unload(time=150, delay=0){
+    $('#loader').delay(delay).fadeOut(time); 
+}
+
+M.AutoInit();
+unload();
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$('#announce').click(function(){
+    $('.tap-target').tapTarget('open');
+    $('#announce').removeClass('pulse');
+});
