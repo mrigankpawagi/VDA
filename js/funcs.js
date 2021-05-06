68dab381697a710c9e642a73d8a8f49531b7e65f
Mrigank Pawagi <mrigankpawagi@gmail.com>
2021-05-06 16:37:05 +0530

    Added Functions + Demo Buttons

---
+++ js/funcs.js
@@ -0,0 +1,59 @@
+function indexedMean(A){
+    var sum = 0;
+    var N =  Object.keys(A).length;
+    Object.keys(A).map((e) => {
+        sum += +e * A[e]
+    });
+    return (sum / N);
+}
+
+function der(list){
+    var latest = list[list.length - 1];
+    var yester = list[list.length - 2];
+    var yestyester = list[list.length - 3];
+    var firstder = latest - yester;
+    var secondder = firstder - (yester - yestyester);
+    return [latest, firstder, secondder];
+}
+
+function vulnerability(Vocations, C_Mean, C_Max, H_Median, H_Max, E_Median, E_Max, Population, Housing, Birth_Rate, Death_Rate, Median_Age, Life_Expectancy, Total_Cases_Country, Max_Daily_Cases, Max_Daily_Deaths, Total_Cases, Cases, Total_Deaths_Country, Total_Deaths, Deaths, R, T){
+    var V = indexedMean(Vocations) / 8;
+    var C = C_Mean / C_Max;
+    var H = H_Median / H_Max;
+    var E = E_Median / E_Max;
+
+    var P = Population / 80000;
+    var S = indexedMean(Housing) / 4;
+
+    var Cases_perday, Cases_firstder, Cases_secondder;
+    [Cases_perday, Cases_firstder, Cases_secondder] = der(Cases);
+    var Deaths_perday, Deaths_firstder, Deaths_secondder;
+    [Deaths_perday, Deaths_firstder, Deaths_secondder] = der(Deaths);
+
+    Total_Cases = Total_Cases / Total_Cases_Country;
+    Total_Deaths = Total_Deaths / Total_Deaths_Country;
+    [Cases_perday, Cases_firstder, Cases_secondder] = [Cases_perday / Max_Daily_Cases, Cases_firstder / Max_Daily_Cases, Cases_secondder / Max_Daily_Cases];
+    [Deaths_perday, Deaths_firstder, Deaths_secondder] = [Deaths_perday / Max_Daily_Deaths, Deaths_firstder / Max_Daily_Deaths, Deaths_secondder / Max_Daily_Deaths];
+    
+    var B = Birth_Rate / 50;
+    var D = Death_Rate / 50;
+    var A = Median_Age / Life_Expectancy;
+    T = T/3;
+
+    return ((V/C/H/E) + (D * A / B) + (P / S) + (Total_Cases * Cases_perday * Cases_firstder * Cases_secondder) * (Total_Deaths * Deaths_perday * Deaths_firstder * Deaths_secondder) / R / T) / 4;
+}
+
+function logisticalFeasibility(K, t, L, Area_City, Area_Country, Max_Traffic_Speed){
+    var G = Area_City / Area_Country;
+    t = t / Max_Traffic_Speed;
+    L = L / 2000; // km per 100 sq km
+    K = K / 10; // Expecting 10 to be a possible maximum
+
+    return (L/G + K/t) / 2;
+
+}
+
+
+function delayTolerance(){  
+    return;
+}
--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ $('#announce').click(function(){
     $('.tap-target').tapTarget('open');
     $('#announce').removeClass('pulse');
 });
+
+/* $.ajax({
+    type:'POST',
+    url: '',
+    success: function(r){
+
+    }
+}); */
+
+$("#selectCities a.btn-small").click(function(){
+    $(this).toggleClass("selected");
+});
